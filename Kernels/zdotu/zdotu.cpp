390fb0d13fb6bd533c1a4aeaaf21d425a87f3ae3
gilles fourestey <gilles.fourestey@epfl.ch>
2016-05-24 14:44:05 +0200

    added copyright

--- Kernels/zdotu/zdotu.cpp
+++ Kernels/zdotu/zdotu.cpp
@@ -1,3 +1,10 @@
+// * author gilles fourestey gilles.fourestey@epfl.ch
+// * Copyright 2015. All rights reserved.
+// *
+// * Users are NOT authorized
+// * to employ the present software for their own publications
+// * before getting a written permission from the author of this file.
+
 #include <complex>
 #include <iostream>
 #include <mkl.h>
